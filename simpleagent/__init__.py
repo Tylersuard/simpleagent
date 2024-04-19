717b3a94e03116fcaa8de7014d1d51dd6d55373c
Tyler <41713505+Tylersuard@users.noreply.github.com>
2024-04-19 07:22:03 -0700

    Update __init__.py

--- simpleagent/__init__.py
+++ simpleagent/__init__.py
@@ -56,4 +56,53 @@ class Swarm:
                 # print(f"Response from {agent.agent_name}: \n {agent_response}")
                 chat_history += agent_response
 
-#Something is wrong, it will have each agent talk 5 times before moving on to the next agent.        
+#Something is wrong, it will have each agent talk 5 times before moving on to the next agent. 
+
+
+
+from abc import ABC
+import openai
+import requests
+from groq import Groq
+
+class SendToLLM(ABC):
+    def __init__(self, endpoint, api_key):
+        pass
+    def send(self, data):
+        pass
+
+class SendToOpenai(SendToLLM):
+    def __init__(self, endpoint, api_key):
+        if not api_key:
+            raise ValueError("api_key is required")
+        self.api_key = api_key
+        self.endpoint = endpoint
+
+    def send(self, message):
+        client = openai.OpenAI(api_key=self.api_key)
+        response = client.chat.completions.create(
+            model="gpt-4-0125-preview",
+            messages=[{"role": "system", "content": message}],
+            temperature=0.3,
+            max_tokens=500,
+        )
+        return response.choices[0].message.content
+    
+class SendToGroq(SendToLLM):
+    def __init__(self, endpoint, api_key):
+        self.api_key=api_key
+
+    def send(self, message):
+        client = Groq(api_key=self.api_key)
+        chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": f"{message}",
+        }
+    ],
+    model="mixtral-8x7b-32768",
+)
+
+        return chat_completion.choices[0].message.content
+
