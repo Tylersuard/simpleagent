0bd602366fef64a8b11b53dc83eea48eb66190a0
Tyler <41713505+Tylersuard@users.noreply.github.com>
2024-04-19 07:12:29 -0700

    Update and rename agent_names.py to simpleagent/agent_names.py

--- agent_names.py
+++
@@ -1,2 +0,0 @@
-agent_firstnames = ["Swifty", "Flurry", "Crispy", "Droopy", "Skullcrusher", "Nibbles", "Wobbles", "Zesty", "Cranky", "Flappy", "Mopey"]
-agent_lastnames = ["McGrew", "McSandwichTime", "McRuffinFellow","Crankypants", "Muffintop", "Ogelthorpe", "Noodlebuns", "Wafflestomp", "Quizzler", "Arsonpants"]
---
+++ simpleagent/agent_names.py
@@ -0,0 +1,2 @@
+agent_firstnames = ["Swifty", "Flurry", "Crispy", "Droopy", "Skullcrusher", "Nibbles", "Wobbles", "Zesty", "Cranky", "Flappy", "Mopey"]
+agent_lastnames = ["McGrew", "McSandwichTime", "McRuffinFellow","Crankypants", "Muffintop", "Ogelthorpe", "Noodlebuns", "Wafflestomp", "Quizzler", "Arsonpants"]
