fcddbed45b95d0bb478b82944b551173d0b78ab5
maelghrib <maelghrib@gmail.com>
2024-04-22 02:12:36 +0200

    Added support for choosing model name

--- simpleagent/utils.py
+++ simpleagent/utils.py
@@ -4,31 +4,33 @@ import requests
 from groq import Groq
 
 class SendToLLM(ABC):
-    def __init__(self, endpoint, api_key):
+    def __init__(self, endpoint, api_key, model):
         pass
     def send(self, data):
         pass
 
 class SendToOpenai(SendToLLM):
-    def __init__(self, endpoint, api_key):
+    def __init__(self, endpoint, api_key, model="gpt-4-0125-preview"):
         if not api_key:
             raise ValueError("api_key is required")
         self.api_key = api_key
         self.endpoint = endpoint
+        self.model = model
 
     def send(self, message):
         client = openai.OpenAI(api_key=self.api_key)
         response = client.chat.completions.create(
-            model="gpt-4-0125-preview",
+            model=self.model,
             messages=[{"role": "system", "content": message}],
             temperature=0.3,
             max_tokens=500,
         )
         return response.choices[0].message.content
-    
+
 class SendToGroq(SendToLLM):
-    def __init__(self, endpoint, api_key):
+    def __init__(self, endpoint, api_key, model="mixtral-8x7b-32768"):
         self.api_key=api_key
+        self.model=model
 
     def send(self, message):
         client = Groq(api_key=self.api_key)
@@ -39,7 +41,7 @@ class SendToGroq(SendToLLM):
             "content": f"{message}",
         }
     ],
-    model="mixtral-8x7b-32768",
+    model=self.model,
 )
 
         return chat_completion.choices[0].message.content
