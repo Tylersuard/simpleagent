a59af42b29f0694f85a1acefef253caf4c55671a
Jonathan Adithya <joadithya39@gmail.com>
2024-05-04 02:28:38 -0700

    feat: update send methods to incorporate the system and user message

--- simpleagent/utils.py
+++ simpleagent/utils.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 import openai
 from groq import Groq
@@ -10,23 +10,24 @@ class MODELS:
 
 
 class SendToLLM(ABC):
-    def __init__(self, endpoint, api_key, model):
-        pass
-
-    def send(self, message, system="You are a helpful assistant."):
-        pass
-
-
-class SendToOpenai(SendToLLM):
-    def __init__(self, endpoint, api_key, model=MODELS.GPT_4_PREVIEW):
-        if not api_key:
+    def __init__(self, endpoint: str, api_key: str, model: str):
+        if api_key is None:
             raise ValueError("api_key is required")
         self.api_key = api_key
         self.endpoint = endpoint
         self.model = model
+
+    @abstractmethod
+    def send(self, message, system="You are a helpful assistant.") -> str:
+        raise NotImplementedError("send method must be implemented.")
+
+
+class SendToOpenai(SendToLLM):
+    def __init__(self, endpoint: str, api_key: str, model: str = MODELS.GPT_4_PREVIEW):
+        super().__init__(endpoint, api_key, model)
         self.client = openai.OpenAI(api_key=self.api_key)
 
-    def send(self, message, system="You are a helpful assistant."):
+    def send(self, message, system="You are a helpful assistant.") -> str:
         response = self.client.chat.completions.create(
             model=self.model,
             messages=[
@@ -36,25 +37,26 @@ class SendToOpenai(SendToLLM):
             temperature=0.3,
             max_tokens=500,
         )
+
+        if response.choices[0].message.content is None:
+            return "I am sorry, I do not have an answer for that."
         return response.choices[0].message.content
 
 
 class SendToGroq(SendToLLM):
     def __init__(self, endpoint, api_key, model=MODELS.MIXTRAL_8X_7B):
-        self.api_key = api_key
-        self.model = model
+        super().__init__(endpoint, api_key, model)
         self.client = Groq(api_key=self.api_key)
 
-    def send(self, message, system="You are a helpful assistant."):
+    def send(self, message, system="You are a helpful assistant.") -> str:
         response = self.client.chat.completions.create(
             messages=[
                 {"role": "system", "content": system},
-                {
-                    "role": "user",
-                    "content": message,
-                },
+                {"role": "user", "content": message},
             ],
             model=self.model,
         )
 
+        if response.choices[0].message.content is None:
+            return "I am sorry, I do not have an answer for that."
         return response.choices[0].message.content
